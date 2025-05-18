5ab17da9d88f23973a03a462ff6416d5d0feb3f8
Nibir2405 <nibirislam56@gmail.com>
2025-05-19 01:45:35 +0600

    Implement job application form submission and database integration

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+@app.route("/", methods =["GET","POST"])
 
 def index():
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        date = request.form.get("date")
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        occupation = request.form.get("occupation")
+        
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+
     return render_template("index.html")
 
-app.run(debug=True,port=5500)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True,port=5500)
