9738a4550b899371912c5875667e7aaf5f30764b
Nibir2405 <nibirislam56@gmail.com>
2025-05-19 16:33:10 +0600

    Add email notification for job application submission

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "navidulislam2002@gmail.com"
+app.config["MAIL_PASSWORD"] = "mmgm rayj zahb mezk"
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 class Form(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,6 +38,17 @@ def index():
         form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"Thank you for your submission, {first_name}."\
+                       f"Here is your data:\n {first_name}\n{last_name}\n{date}\n" \
+                       f"Make sure to present on the available date"
+        message = Message(subject="Job Form Submission", 
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        
+        mail.send(message)
+
         flash("Your application was submitted successfully", "success")
 
     return render_template("index.html")
